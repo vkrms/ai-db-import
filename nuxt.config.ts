579bc2b504d5e89d400e40650ee15a287ec90e40
Pavel M <april504@yandex.ru>
2026-05-01 12:29:53 +0700

    chore: set Nuxt dev server port to 7151

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  devServer: {
+    port: 7151,
+  },
   runtimeConfig: {
     openrouterApiKey: process.env.OPENROUTER_API_KEY,
   },
